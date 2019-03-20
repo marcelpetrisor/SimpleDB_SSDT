5161df0e77f292853052cd7dd9f3e1e1c07e41fd
Marcel Petrisor <Marcel.Petrisor@wpa.org.uk>
2019-03-20 12:36:31 +0200

    Add project files.

---
+++ SimpleDB_SSDT_Dev/dbo/Tables/customer.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[customer] (
+    [customer_id] INT          IDENTITY (1, 1) NOT NULL,
+    [first_name]  VARCHAR (50) NOT NULL,
+    [last_name]   VARCHAR (50) NOT NULL,
+    [age]         TINYINT      NOT NULL,
+    [zip_code]    VARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_customer] PRIMARY KEY CLUSTERED ([customer_id] ASC)
+);
+
