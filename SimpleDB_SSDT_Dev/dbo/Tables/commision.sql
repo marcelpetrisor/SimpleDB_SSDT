ba4b8fe16b6b83385cfff77172912dbc18ef1d32
Marcel Petrisor <Marcel.Petrisor@wpa.org.uk>
2019-03-20 13:24:33 +0200

    Commision_7435 , Add table customer_type , Changed commision.type to INT from VARCHAR

--- SimpleDB_SSDT_Dev/dbo/Tables/commision.sql
+++ SimpleDB_SSDT_Dev/dbo/Tables/commision.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[commision]
 (
 [commision_id] [int] NOT NULL,
-[type] [varchar] (50) NOT NULL,
+[type] [int] NOT NULL,
 [percentage] [tinyint] NOT NULL, 
     CONSTRAINT [PK_commision] PRIMARY KEY ([commision_id])
 )
---
+++ SimpleDB_SSDT_Dev/dbo/Tables/customer_type.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[customer_type]
+(
+[customer_type_id] [int] NOT NULL IDENTITY(1, 1),
+[type] [varchar] (50) NULL,
+[type_id] [int] NOT NULL, 
+    CONSTRAINT [PK_customer_type] PRIMARY KEY ([customer_type_id]) 
+)
