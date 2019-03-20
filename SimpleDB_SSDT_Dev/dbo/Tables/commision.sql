09190bb2e402eed269124e610c8c1ae241654990
Marcel Petrisor <Marcel.Petrisor@wpa.org.uk>
2019-03-20 13:00:48 +0200

    Add table commision , development Commision_7435

---
+++ SimpleDB_SSDT_Dev/dbo/Tables/commision.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[commision]
+(
+[commision_id] [int] NOT NULL,
+[type] [varchar] (50) NOT NULL,
+[percentage] [tinyint] NOT NULL, 
+    CONSTRAINT [PK_commision] PRIMARY KEY ([commision_id])
+)
