ab2faa0da1c6187432b5fa17c730f5a25eefb268
Marcel Petrisor <Marcel.Petrisor@wpa.org.uk>
2019-03-20 15:25:36 +0200

    Commision removed from RC

--- SimpleDB_SSDT_Dev/Script.PostDeployment1.sql
+++ SimpleDB_SSDT_Dev/Script.PostDeployment1.sql
@@ -9,21 +9,3 @@ Post-Deployment Script Template
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
-IF (SELECT COUNT(*)
-    FROM   [dbo].[customer_type]) = 0
-    BEGIN
-        PRINT (N'Add 4 rows to [dbo].[customer_type]');
-        SET IDENTITY_INSERT [dbo].[customer_type] ON;
-        INSERT  INTO [dbo].[customer_type] ([customer_type_id], [type], [type_id])
-        VALUES                            (1, 'individual', 1);
-        INSERT  INTO [dbo].[customer_type] ([customer_type_id], [type], [type_id])
-        VALUES                            (2, 'group', 2);
-        INSERT  INTO [dbo].[customer_type] ([customer_type_id], [type], [type_id])
-        VALUES                            (3, 'intermediary', 3);
-        INSERT  INTO [dbo].[customer_type] ([customer_type_id], [type], [type_id])
-        VALUES                            (4, 'hp', 4);
-        SET IDENTITY_INSERT [dbo].[customer_type] OFF;
-    END
-
-
-GO
--- SimpleDB_SSDT_Dev/dbo/Tables/commision.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[commision]
-(
-[commision_id] [int] NOT NULL,
-[type] [int] NOT NULL,
-[percentage] [tinyint] NOT NULL, 
-    CONSTRAINT [PK_commision] PRIMARY KEY ([commision_id])
-)
