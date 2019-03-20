8a72eb3e5a37c003c4e4eaf53f69a5ffb0b87116
Marcel Petrisor <Marcel.Petrisor@wpa.org.uk>
2019-03-20 12:53:39 +0200

    Add Table ErrorLog

---
+++ SimpleDB_SSDT_Dev/dbo/Tables/ErrorLog.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[ErrorLog] (
+    [ErrorLogKey]     INT           IDENTITY (1, 1) NOT NULL,
+    [ErrorLogDate]    DATETIME      NULL DEFAULT SYSDATETIME(),
+    [ErrorLogMessage] VARCHAR (MAX) NULL,
+    CONSTRAINT [ErrorLogPK] PRIMARY KEY CLUSTERED ([ErrorLogKey] ASC)
+)
